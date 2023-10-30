6aba14755badcace9700e2de64761c0456dda2fe
Thriven1995 <115354088+Thriven1995@users.noreply.github.com>
2023-10-30 19:42:05 +0530

    Add files via upload

---
+++ Lab_001_Hello World.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print(4 + 9)
+print(9 * 4)
+print("Triven","Kumar","Punati", sep="/")
+_name = "Triven Kumar"
+print(_name.title())
+print(_name.upper())
+print(_name.lower())
+import keyword
+print(keyword.kwlist)
+name = "Triven"
+print(name)
+name = "Kumar"
+print(name)
+
+
